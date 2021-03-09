9fef138bf49268ad2860462bbc3c3a2bb897c23e
FahadBurney <fahadburney112@gmail.com>
2021-03-09 22:29:37 +0530

    I have changed the layout of main Activity

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -8,57 +8,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    CardView incomeCard,expenseCard,transactionsCard,reportsCard;
+    LinearLayout incomeLayout, expenseLayout, transactionsLayout, reportsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        incomeCard=findViewById(R.id.incomeCard);
-        expenseCard=findViewById(R.id.expenseCard);
-        transactionsCard=findViewById(R.id.transactionsCard);
-        reportsCard=findViewById(R.id.reportsCard);
-        incomeCard.setOnClickListener(this);
-        expenseCard.setOnClickListener(this);
-        transactionsCard.setOnClickListener(this);
-        reportsCard.setOnClickListener(this);
-        }
+        incomeLayout = findViewById(R.id.incomeLayout);
+        expenseLayout = findViewById(R.id.expenseLayout);
+        transactionsLayout = findViewById(R.id.transactionsLayout);
+        reportsLayout = findViewById(R.id.reportsLayout);
+        incomeLayout.setOnClickListener(this);
+        expenseLayout.setOnClickListener(this);
+        transactionsLayout.setOnClickListener(this);
+        reportsLayout.setOnClickListener(this);
+    }
 
     @Override
-    public void onClick(View view)
-    {
-switch(view.getId())
-{
-    case R.id.incomeCard:
-    {
-        Intent intent=new Intent(this, AddEditIncomeActivity.class);
-        startActivity(intent);
-        break;
-    }
-    case  R.id.expenseCard:
-    {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.incomeLayout: {
+                Intent intent = new Intent(this, AddEditIncomeActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.expenseLayout: {
 
-        Intent intent=new Intent(this, AddEditExpenseActivity.class);
-        startActivity(intent);
-        break;
-    }
-    case   R.id.transactionsCard:
-    {
-       Intent intent=new Intent(this,ShowTransactionsActivity.class);
-       startActivity(intent);
-       break;
-    }
-    case  R.id.reportsCard:
-    {
-        Intent intent=new Intent(this,ReportsActivity.class);
-        startActivity(intent);
-        break;
-    }
-}
+                Intent intent = new Intent(this, AddEditExpenseActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.transactionsLayout: {
+                Intent intent = new Intent(this, ShowTransactionsActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.reportsLayout: {
+                Intent intent = new Intent(this, ReportsActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
 
     }
 }
